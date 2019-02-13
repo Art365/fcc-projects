f06cf6c9410bb3de058c2a7b8b087bf1e682a724
Artem Petrov <me@artempetrov.com>
2019-02-12 20:30:48 -0500

    Modified canvas-related js.

--- Tribute Page/app.js
+++ Tribute Page/app.js
@@ -1,106 +1,107 @@
-// have a card featuring different quotes. Have them cycle.
-
-
-
 window.onload = function() {
   "use strict";
-  const bgColor = "black";
-  var porthole = document.getElementById("porthole");
-  var canvas = document.getElementById("c");
-  var offCanvas = document.createElement("canvas");
-  var canvasWidth = canvas.width = offCanvas.width = porthole.offsetWidth;
-  var canvasHeight = canvas.height = offCanvas.height = porthole.offsetHeight;
-  var STAR_COUNT = 800;
-  var canvasCenter = { x: canvasWidth / 2, y: canvasHeight / 2};
-  var minStarRadius = Math.max(canvasWidth, canvasHeight);
-  var maxStarRadius = Math.max(canvasWidth, canvasHeight) * 10;
-  var ctx = canvas.getContext("2d", { alpha: false });
-  var offCtx = offCanvas.getContext("2d", { alpha: false });
-  const round = (x) => Math.round(x);
-  const randomInt = (x) => Math.round(Math.random() * x);
-  const randomInRange = (min, max) => min + Math.random() * (max - min);
+  const BG_COLOR = "black",
+        STAR_COUNT = 1000,
+        round = (x) => Math.round(x),
+        randomInt = (x) => Math.round(Math.random() * x),
+        randomInRange = (min, max) => min + Math.random() * (max - min);
+  let canvas = document.getElementById("c"),
+      offCanvas = document.createElement("canvas"),
+      canvasWidth = canvas.width = offCanvas.width = document.body.offsetWidth,
+      canvasHeight = canvas.height = offCanvas.height = document.body.offsetHeight,
+      canvasCenter = { x: canvasWidth / 2, y: canvasHeight / 2},
+      minStarRadius = Math.max(canvasWidth, canvasHeight),
+      maxStarRadius = minStarRadius * 20,
+      minDistance = minStarRadius * 3,
+      maxDistance = minStarRadius * 30,
+      speed = 6,
+      ctx = canvas.getContext("2d", { alpha: false }),
+      offCtx = offCanvas.getContext("2d", { alpha: false });
+
   
   window.addEventListener("resize", resizeCanvas, false);
- /* porthole.addEventListener("mousemove", function(event) {
-    canvasCenter.x = event.pageX - canvasWidth / 2;
-    canvasCenter.y = event.pageY - canvasHeight / 2;
-  }, false); */
 
-  function Star(x, y, r, distance, color) {
-    this.x = x;
-    this.y = y;
-    this.r = r;
-    this.distance = distance;
-    this.ar = this.r / this.distance;
-    this.color = color;
+
+  class Star {
+	  constructor(x, y, r, distance, color) {
+	    this.x = x;
+      this.y = y;
+      this.r = r;
+      this.distance = distance;
+      this.ar = r / distance;
+      this.color = color;
+	  }
+	
+	  draw(context) {
+      context.beginPath();
+      context.moveTo(this.x, this.y);
+      context.arc(this.x, this.y, this.ar, 0, Math.PI * 2, false);
+      context.fillStyle = this.color;
+      context.fill();
+    }
+	
+	  update() {
+	    this.ar += this.ar / this.distance;
+      this.distance -= speed;
+
+      if (this.x < 0 || this.x > canvasWidth || this.y < 0 || this.y > canvasHeight || this.distance < 0) {
+        this.x = randomInt(canvasWidth);
+        this.y = randomInt(canvasHeight);
+        this.r = randomInRange(minStarRadius, maxStarRadius);
+        this.distance = maxDistance;
+        this.ar = this.r / this.distance;
+      } else {
+        this.x += (this.x - canvasCenter.x) / this.distance;
+        this.y += (this.y - canvasCenter.y) / this.distance;
+      }
+	  }
+	
   }
   
-  Star.prototype.draw = function(context) {
-    context.beginPath();
-    context.moveTo(this.x, this.y);
-    context.arc(this.x, this.y, this.ar, 0, Math.PI * 2, false);
-    context.fillStyle = this.color;
-    context.fill();
-  }
-
-  Star.prototype.update = function() {
-    this.ar += this.ar / this.distance;
-    this.distance -= 4;
-
-    if (this.x < 0 || this.x > canvasWidth || this.y < 0 || this.y > canvasHeight || this.distance < 1) {
-      this.x = randomInt(canvasWidth);
-      this.y = randomInt(canvasHeight);
-      this.r = randomInRange(minStarRadius, maxStarRadius);
-      this.distance = randomInRange(5000, 10000);
-      this.ar = this.r / this.distance;
-    } else {
-      this.x += (this.x - canvasCenter.x) / this.distance;
-      this.y += (this.y - canvasCenter.y) / this.distance;
-    }
-  }
 
   //initialize some stars
   function createStars(num) {
-    let stars = [];
+    let starsArr = [];
     for (let i = 0; i < num; i++) {
-      let x = randomInt(canvasWidth * 100);
-      let y = randomInt(canvasHeight * 100);
+      let x = randomInt(canvasWidth);
+      let y = randomInt(canvasHeight);
       let radius = randomInRange(minStarRadius, maxStarRadius);
-      let distance = randomInt(10000);
-      let red = randomInRange(200,255);
-      let green = randomInRange(200,255);
-      let blue = randomInRange(200,255);
+      let distance = randomInRange(minDistance, maxDistance);
+      let [red, green, blue] = [randomInRange(198,255), randomInRange(198,255), randomInRange(198,255)];
       green = Math.min(red, green, blue);
       let color = `rgba(${red},${green},${blue})`;
-      stars[i] = new Star(x, y, radius, distance, color);
+      starsArr[i] = new Star(x, y, radius, distance, color);
     }
-    return stars;
+    return starsArr;
   }
   
-  var stars = createStars(STAR_COUNT);
+  
+  let stars = createStars(STAR_COUNT);
 
   function resizeCanvas() {
-    canvasWidth = porthole.offsetWidth;
-    canvasHeight = porthole.offsetHeight;
+    canvasWidth = document.body.offsetWidth;
+    canvasHeight = document.body.offsetHeight;
     canvasCenter = { x: canvasWidth / 2, y: canvasHeight / 2};
     canvas.width = offCanvas.width = canvasWidth;
     canvas.height = offCanvas.height = canvasHeight;
     ctx = canvas.getContext("2d", { alpha: false });
     offCtx = offCanvas.getContext("2d", { alpha: false });
     minStarRadius = Math.max(canvasWidth, canvasHeight);
-    maxStarRadius = Math.max(canvasWidth, canvasHeight) * 10;
+    maxStarRadius = minStarRadius * 20;
+    minDistance = minStarRadius * 3;
+    maxDistance = minStarRadius * 30,
+    speed = 6,
     stars = createStars(STAR_COUNT);
   }
   
 
-
   function animate() {
-    offCtx.fillStyle = bgColor;
+    offCtx.fillStyle = BG_COLOR;
     offCtx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-    for (var i = 0, len = stars.length; i < len; i++) {
-      stars[i].update();
+    for (let i = 0, len = stars.length; i < len; i++) {
       stars[i].draw(offCtx);
+      stars[i].update();
     }
     
     ctx.save();
@@ -110,6 +111,6 @@ window.onload = function() {
     window.requestAnimationFrame(animate);
   }
 
-  // animate();
+  animate();
 
 }
