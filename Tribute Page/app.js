22f3944b0a6b19a6eaae657fbb845f840364a16e
Artem Petrov <me@artempetrov.com>
2019-03-23 15:58:13 -0400

    almost finished the slideshow

--- Tribute Page/app.js
+++ Tribute Page/app.js
@@ -34,77 +34,82 @@ window.onload = function() {
       
   init();
   
-  const imagesOfCarl = [
-    [
-      "assets/images/sagan_in_front_of_planets.jpg 600w, assets/images/sagan_in_front_of_planets--large.jpg 1200w", 
-      "Carl Sagan standing in front of planets.", 
-      `<p><cite>NASA JPL.</cite></p>
-      <br>
-      <p>Carl Sagan standing in front of planets.</p>`
-    ],
-    [
-      "assets/images/planetary_society.jpg 976w, assets/images/planetary_society--large.jpg 1952w", 
-      "Planetary Society.", 
-      `<p><cite>NASA JPL. <i>Planetary Society.</i> , 1970s. Photograph.</cite></p>
-      <br>
-      <p>Carl Sagan, Bruce Murray (seated right to left) and (standing) Louis Friedman (standing, right), 
-      the founders of The Planetary Society at the time of signing the papers formally incorporating the organization. 
-      The fourth person is Harry Ashmore(standing, left), an advisor, who greatly helped in the founding of the Society.</span></p>`
-    ],
-    [
-      "assets/images/sagan_with_planets.jpg 572w, assets/images/sagan_with_planets--large.jpg 1144w", 
-      "Carl Sagan on the set of Cosmos.", 
-      `<p><cite>Castaneda, Eduardo. <i>Carl Sagan with the planets.</i> , 1981. Photograph. https://www.loc.gov/item/cosmos000104/.</cite></p>
-      <br>
-      <p>Carl Sagan on the set of the television program Cosmos: A personal journey, standing among scale models of several 
-      planets in the solar system.</p>`
-    ],
-    ["assets/images/carl_pioneer.jpg 640w, assets/images/carl_pioneer--large.jpg 1280w", 
-    "Carl Sagan holding one of the Pioneer plaques.", 
-    `<p><cite>Sagan, Carl. "A Message from Earth." Science Magazine, February 25, 1972.</cite></p>
-    <br>
-    <p>Carl Sagan holding one of the Pioneer plaques he helped design. The plaques were 
-    meant to communicate information about the origin of the spacecraft to potential intelligent extrasterrestrial interceptor.
-    The two plaques were placed on board the 1972 Pioneer 10 and 1973 Pioneer 11 spacecraft.</p>`
-  ],
-  ];
+
+
   
+function updateHero() {
+  render = 1;
+  updateHero.prototype.imageClassList.add("fade-out");
+  updateHero.prototype.imageCaptionClassList.remove("slide-out");
+  setTimeout(function(e) {
+    updateHero.prototype.showNextSlide();
+  }, 1000);
+}
+
+updateHero.prototype = {
+  index: 0,
+  image: document.getElementById("image"),
+  imageClassList: document.getElementById("image").classList,
+  imageCaption: document.getElementById("img-caption"),
+  imageCaptionClassList: document.getElementById("img-caption").classList,
+  citation: document.getElementById("hero-citation"),
+  description: document.getElementById("hero-description"),
+
+  heroImages: [
+    {
+      srcset: "assets/images/sagan_in_front_of_planets.jpg 600w, assets/images/sagan_in_front_of_planets--large.jpg 1200w", 
+      alt: "Carl Sagan standing in front of planets.", 
+      citation: "NASA JPL.",
+      description: "Carl Sagan standing in front of planets."
+    },
+    {
+      srcset: "assets/images/planetary_society.jpg 976w, assets/images/planetary_society--large.jpg 1952w", 
+      alt: "Planetary Society.", 
+      citation: "NASA JPL. Planetary Society. , 1970s. Photograph.",
+      description: "Carl Sagan, Bruce Murray (seated right to left) and (standing) Louis Friedman (standing, right), the founders of The Planetary Society at the time of signing the papers formally incorporating the organization. The fourth person is Harry Ashmore(standing, left), an advisor, who greatly helped in the founding of the Society."
+    },
+    {
+      srcset: "assets/images/sagan_with_planets.jpg 572w, assets/images/sagan_with_planets--large.jpg 1144w", 
+      alt: "Carl Sagan on the set of Cosmos.", 
+      citation: "Castaneda, Eduardo. Carl Sagan with the planets. , 1981. Photograph. https://www.loc.gov/item/cosmos000104/.",
+      description: "Carl Sagan on the set of the television program Cosmos: A personal journey, standing among scale models of several planets in the solar system."
+    },
+    {
+      srcset: "assets/images/carl_pioneer.jpg 640w, assets/images/carl_pioneer--large.jpg 1280w", 
+      alt: "Carl Sagan holding one of the Pioneer plaques.", 
+      citation: "Sagan, Carl. \"A Message from Earth.\" Science Magazine, February 25, 1972.",
+      description: "Carl Sagan holding one of the Pioneer plaques he helped design. The plaques were meant to communicate information about the origin of the spacecraft to potential intelligent extrasterrestrial interceptor. The two plaques were placed on board the 1972 Pioneer 10 and 1973 Pioneer 11 spacecraft."
+    },
+  ],
+
+  showNextSlide: function() {
+    "use strict"
+    updateHero.prototype.index = (updateHero.prototype.index + 1) % updateHero.prototype.heroImages.length;
+    updateHero.prototype.image.setAttribute("srcset", updateHero.prototype.heroImages[updateHero.prototype.index].srcset);
+    updateHero.prototype.image.setAttribute("alt", updateHero.prototype.heroImages[updateHero.prototype.index].alt);
+    updateHero.prototype.citation.textContent = updateHero.prototype.heroImages[updateHero.prototype.index].citation; 
+    updateHero.prototype.description.textContent = updateHero.prototype.heroImages[updateHero.prototype.index].description;
+    updateHero.prototype.imageClassList.remove("fade-out");
+    updateHero.prototype.imageCaptionClassList.add("slide-out");
+    render = 0;
+  }
+}
+
+
 
   // add event listener for resize
   window.addEventListener("resize", resizeCanvas, false);
 
-  // add a sentinel value that turns off canvas drawing when images are being shown
-  let count = 0;
-  const image = document.getElementById("image");
-  const imageClassList = image.classList;
-  const imageCaption = document.getElementById("img-caption");
-  const imageCaptionClassList = imageCaption.classList;
-
-
-
-  // replace innerHTML with content replacemenet. that way, browser doesnt have to re-render new elemenets.
-  setInterval(() => {
-    render = 1;
-    imageClassList.add("fade-out");
-    imageCaptionClassList.add("slide-off");
-    count = (count + 1) % imagesOfCarl.length;
-    setTimeout(() => {
-      image.setAttribute("srcset", imagesOfCarl[count][0]);
-      image.setAttribute("alt", imagesOfCarl[count][1]);
-      // imageCaption.innerHTML = imagesOfCarl[count][2];       // replace this! instead replace content.
-      imageClassList.remove("fade-out");
-      imageCaptionClassList.remove("slide-off");
-      setTimeout(() => render = 0, 1000);
-    }, 1000);
-  }, 8000);
+  
+  setInterval(updateHero, 10000);
 
   // image.addEventListener("click", function(e) {
   //   let imageClassList = e.target.classList;
   //   imageClassList.toggle("fade-out");
-  //   count = (count + 1) % imagesOfCarl.length;
+  //   count = (count + 1) % heroImages.length;
   //   setTimeout(function() {
-  //     e.target.setAttribute("srcset", imagesOfCarl[count][0]);
-  //     e.target.setAttribute("alt", imagesOfCarl[count][1]);
+  //     e.target.setAttribute("srcset", heroImages[count][0]);
+  //     e.target.setAttribute("alt", heroImages[count][1]);
   //     imageClassList.toggle("fade-out");
   //   }, 800);
   // }, false);
@@ -167,7 +172,7 @@ window.onload = function() {
   
 
   function animate() {
-    
+
     if (render) {
       offCtx.fillStyle = BG_COLOR;
       offCtx.fillRect(0, 0, canvasWidth, canvasHeight);
@@ -188,6 +193,5 @@ window.onload = function() {
   }
 
   animate();
-
 
 }
